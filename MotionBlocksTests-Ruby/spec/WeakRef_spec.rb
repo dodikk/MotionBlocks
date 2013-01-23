8ee9f14695870094ed82a15dad5c3a9529530a34
dodikk <dodikk88.reg@gmail.com>
2013-01-23 15:02:51 +0200

    Added a memory management test

---
+++ MotionBlocksTests-Ruby/spec/WeakRef_spec.rb
@@ -0,0 +1,39 @@
+describe "RMWeakRef memory management" do
+  before do
+
+    # NSAutoreleasePool pool = NSAutoreleasePool.alloc.init
+
+    hello = 'hello'
+    world = 'world'
+      
+    @world = world
+    
+    @weakHello = RMWeakRef.alloc.initWithImpl( hello )
+    @weakWorld = RMWeakRef.alloc.initWithImpl( world )
+
+    hello = nil
+    world = nil
+
+    puts '|'
+    puts '@weakHello.class :'
+    puts @weakHello.class
+    puts @weakHello.impl.class
+    puts @weakWorld.class
+    puts @weakWorld.impl.class
+    puts '------'
+
+    # pool.drain
+  end
+
+  it "Returns nil if not stored otherwise" do
+    puts @weakHello.impl.class
+    puts @weakHello.impl.methods.sort
+
+    @weakHello.impl.should.be.equal( nil )
+  end
+
+    
+  it "Returns valid stored object" do
+     @weakWorld.impl.should.be.equal( @world )
+  end
+end
